1b296bc18f64e215a117721ded2c739eacef207a
Thomas Pretterhofer <thomas.Pretterhofer@gmail.com>
2019-06-06 21:17:48 +0200

    added 2 testcases

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -1,11 +1,39 @@
 package at.fhj.iit;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.NoSuchElementException;
 
 public class StringQueueTest {
+	public StringQueue q;
 
+	@Before
+	public void setup() throws Exception {
+		q = new StringQueue(5);
+	}
 
-    /**
-     * TODO Write your own tests
-     */
+	@Test
+	/**
+	 * tries to poll an element from an empty queue.
+	 * 
+	 * @throws Exception
+	 */
+	public void testPollIsNull() throws Exception {
+		assertTrue(q.poll() == null);
+	}
+
+	/**
+	 * tries to remove an element from an empty queue. Must throw
+	 * NoSuchElementException
+	 * 
+	 * @throws Exception
+	 */
+	@Test(expected = NoSuchElementException.class)
+	public void testPopFromEmptyStack() throws Exception {
+		q.remove();
+	}
 
 }
